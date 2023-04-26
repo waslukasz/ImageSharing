dcc8dbf80c00d37ca5a1ebe1525ef224770ce9d8
Mateusz <mateusz.magiera010@gmail.com>
2023-04-26 19:02:22 +0200

    Added some models

---
+++ ApplicationCore/Models/Album.cs
@@ -0,0 +1,20 @@
+﻿using Application_Core.Commons.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Core.Models
+{
+    public class Album : IIdentity<int>
+    {       
+        public int Id { get; set; }
+        //Do ustalenia
+        public int UserId { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+
+    }
+}
---
+++ ApplicationCore/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using Application_Core.Commons.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application_Core.Models
+{
+    public class Comment:IIdentity<int>
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        //Do ustalenia
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+        public string Text { get; set; }    
+    }
+}
