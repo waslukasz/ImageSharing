597f42da41cee3d2498c5a8511d22f7bd4bb55ec
emch43l <michalgood10@gmail.com>
2023-05-21 22:59:46 +0200

    branch inital

---
+++ Infrastructure/EF/Repository/BaseRepository.cs
@@ -0,0 +1,46 @@
+﻿using Application_Core.Common.Identity;
+using Application_Core.Common.Specification;
+using Infrastructure.Database;
+using Infrastructure.EF.Evaluator;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EF.Repository;
+
+public abstract class BaseRepository<TEntity,TKey> : IRepositoryBase<TEntity> where TEntity: class, IUidIdentity<TKey> where TKey: IEquatable<TKey>
+{
+    private readonly ImageSharingDbContext _context;
+
+    protected BaseRepository(ImageSharingDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<TEntity>> GetByCriteria(ISpecification<TEntity> criteria)
+    {
+        return await GetByCriteriaQuery(criteria).ToListAsync();
+    }
+    
+    public IQueryable<TEntity> GetByCriteriaQuery(ISpecification<TEntity> criteria)
+    {
+        return SpecificationToQueryEvaluator<TEntity>.ApplySpecification(
+            _context.Set<TEntity>(),
+            criteria
+        );
+    }
+
+    public async Task<TEntity?> GetByGuid(Guid id)
+    {
+        return await _context.Set<TEntity>().FirstOrDefaultAsync(e => e.Guid == id);
+    }
+
+    public async Task Add(TEntity entity)
+    {
+        await _context.Set<TEntity>().AddAsync(entity);
+    }
+
+    public async Task Remove(TEntity entity)
+    {
+        await Task.FromResult(_context.Set<TEntity>().Remove(entity));
+    }
+
+}
---
+++ Infrastructure/EF/Repository/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿using Application_Core.Common.Specification;
+
+namespace Infrastructure.EF.Repository;
+
+public interface IRepositoryBase<TEntity>
+{
+    Task<IEnumerable<TEntity>> GetByCriteria(ISpecification<TEntity> criteria);
+    IQueryable<TEntity> GetByCriteriaQuery(ISpecification<TEntity> criteria);
+    Task<TEntity?> GetByGuid(Guid id);
+    Task Add(TEntity entity);
+    Task Remove(TEntity entity);
+}
