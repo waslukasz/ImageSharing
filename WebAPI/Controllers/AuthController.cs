3001c5121f15fef2be39cd6b024ccdd687891384
Łukasz Wąś <ljwasiu@gmail.com>
2023-05-22 21:42:00 +0200

    Expanded Authentication controller.

--- WebAPI/Controllers/AuthController.cs
+++ WebAPI/Controllers/AuthController.cs
@@ -1,18 +1,26 @@
+using System.Net;
+using Application_Core.Exception;
+using Infrastructure.EF.Entity;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebAPI.Request;
+using WebAPI.Response;
 using WebAPI.Services.Interfaces;
 
 namespace WebAPI.Controllers
 {
     [ApiController, Route("/api/auth")]
+    [Authorize]
     public class AuthenticationController : ControllerBase
     {
         private readonly IAuthService _authService;
+        private readonly UserManager<UserEntity> _userManager;
 
-        public AuthenticationController(IConfiguration configuration, IAuthService authService)
+        public AuthenticationController(IConfiguration configuration, IAuthService authService, UserManager<UserEntity> userManager)
         {
             _authService = authService;
+            _userManager = userManager;
         }
         
         /// <summary>
@@ -22,9 +30,70 @@ namespace WebAPI.Controllers
         /// <returns></returns>
         [HttpPost("Login")]
         [AllowAnonymous]
-        public async Task<IActionResult> Authenticate([FromBody] LoginUserRequest request)
+        public async Task<IActionResult> Login([FromBody] LoginUserRequest request)
         {
             return Ok(new { token = await _authService.Login(request) });
+        }
+
+        /// <summary>
+        /// Get current account roles
+        /// </summary>
+        /// <remarks>This can only be done by logged in user.</remarks>
+        /// <returns></returns>
+        [HttpGet("GetRoles")]
+        public async Task<GetRolesAccountResponse> GetRoles()
+        {
+            var currentUser = await _userManager.GetUserAsync(HttpContext.User) ?? throw new UserNotFoundException();
+            var result = new GetRolesAccountResponse()
+            {
+                Username = currentUser.UserName,
+                Roles = (await _userManager.GetRolesAsync(currentUser)).ToList()
+            };
+            return result;
+        }
+        
+        /// <summary>
+        /// Get account roles by username
+        /// </summary>
+        /// <remarks>Required Admin rights to get other account roles.</remarks>
+        /// <returns></returns>
+        [HttpGet("GetRoles/{username}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<GetRolesAccountResponse> GetRolesByUser([FromRoute] string username)
+        {
+            var user = await _userManager.FindByNameAsync(username) ?? throw new UserNotFoundException();
+            var result = new GetRolesAccountResponse()
+            {
+                Username = user.UserName,
+                Roles = (await _userManager.GetRolesAsync(user)).ToList()
+            };
+            return result;
+        }
+
+        /// <summary>
+        /// Assign role to account
+        /// </summary>
+        /// <remarks>Required Admin rights to assign role.</remarks>
+        /// <returns></returns>
+        /// <exception cref="BadRequestException">Account already has role.</exception>
+        [HttpPost("AssignRole")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> AssignRole([FromBody] AssignRoleAuthRequest request)
+        {
+            return await _authService.AssignRole(request) ? NoContent() : throw new BadRequestException("Account already in role.", HttpStatusCode.BadRequest);
+        }
+        
+        /// <summary>
+        /// Remove role from account
+        /// </summary>
+        /// <remarks>Required Admin rights to remove role.</remarks>
+        /// <returns></returns>
+        /// <exception cref="BadRequestException">Account did not had this role.</exception>
+        [HttpPost("RemoveRole")]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> RemoveRole([FromBody] RemoveRoleAuthRequest request)
+        {
+            return await _authService.RemoveRole(request) ? NoContent() : throw new BadRequestException("Account did not had this role.", HttpStatusCode.BadRequest);
         }
     }
 }
--- WebAPI/Services/AuthService.cs
+++ WebAPI/Services/AuthService.cs
@@ -6,7 +6,9 @@ using Application_Core.Exception;
 using Infrastructure.EF.Entity;
 using JWT.Algorithms;
 using JWT.Builder;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using WebAPI.Configuration;
 using WebAPI.Request;
 using WebAPI.Services.Interfaces;
@@ -29,6 +31,26 @@ public class AuthService : IAuthService
     {
         var user = await _userManager.FindByNameAsync(request.Username);
         return await _userManager.CheckPasswordAsync(user, request.Password) ? CreateToken(user) : throw new BadRequestException("Invalid username or password.", HttpStatusCode.BadRequest);
+    }
+
+    public async Task<bool> AssignRole([FromBody] AssignRoleAuthRequest request)
+    {
+        var user = await _userManager.FindByNameAsync(request.Username) ?? throw new UserNotFoundException();
+        if ((await _userManager.AddToRoleAsync(user, request.Role)).Succeeded)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public async Task<bool> RemoveRole(RemoveRoleAuthRequest request)
+    {
+        var user = await _userManager.FindByNameAsync(request.Username) ?? throw new UserNotFoundException();
+        if ((await _userManager.RemoveFromRoleAsync(user, request.Role)).Succeeded)
+        {
+            return true;
+        }
+        return false;
     }
 
     public string CreateToken(UserEntity user)
